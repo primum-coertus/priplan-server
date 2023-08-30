a5c355a5a0df9cc57f22080a9c40a8a70012f0ae
NaufalK25 <naufalkateni2001@gmail.com>
2023-08-30 22:37:25 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const baseUrl = process.env.BASE_URL || `http://localhost:${port}`;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at ${baseUrl}`);
+});
