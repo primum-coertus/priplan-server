c46233563a3392b250de52828ab3f0f80355fbbe
aditiyars <aditiyaramadhansaputra08@gmail.com>
2023-09-01 00:25:31 +0700

    add create plan feature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const model = require('./model');
+
 require('dotenv').config();
 
 const app = express();
@@ -7,9 +9,30 @@ const port = process.env.PORT || 3000;
 const baseUrl = process.env.BASE_URL || `http://localhost:${port}`;
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/priplan');
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+app.post('/', async (req, res) => {
+  const getTitle = req.body.title
+  const getPlan = req.body.plan
+  const getStart_date = req.body.start_date
+  const getEnd_date = req.body.end_date
+  const getIs_completed = req.body.is_completed
+
+  const planDocument = {
+    title:getTitle,
+    plan:getPlan,
+    "start_date":getStart_date,
+    "end_date":getEnd_date,
+    is_completed:getIs_completed
+  }
+
+  await model.insertMany([planDocument])
+  res.send("berhasil menambahkan rencana")
+})
 
 app.listen(port, () => {
     console.log(`Server running at ${baseUrl}`);
