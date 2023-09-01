8028f79ffda0a7d6ca0ed938bbdb379a40dbfe04
NaufalK25 <naufalkateni2001@gmail.com>
2023-09-01 10:53:53 +0700

    feat: change response to json

--- src/index.js
+++ src/index.js
@@ -9,44 +9,48 @@ const port = process.env.PORT || 3000;
 const baseUrl = process.env.BASE_URL || `http://localhost:${port}`;
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/priplan');
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.send('Hello World');
 });
 
-app.post('/', async (req, res) => {
-  const getTitle = req.body.title
-  const getPlan = req.body.plan
-  const getStart_date = req.body.start_date
-  const getEnd_date = req.body.end_date
-  const getIs_completed = req.body.is_completed
+app.post('/create', async (req, res) => {
+  const getTitle = req.body.title;
+  const getPlan = req.body.plan;
+  const getStart_date = req.body.start_date;
+  const getEnd_date = req.body.end_date;
+  const getIs_completed = req.body.is_completed;
 
   const planDocument = {
-    title:getTitle,
-    plan:getPlan,
-    "start_date":getStart_date,
-    "end_date":getEnd_date,
-    is_completed:getIs_completed
-  }
+    title: getTitle,
+    plan: getPlan,
+    start_date: getStart_date,
+    end_date: getEnd_date,
+    is_completed: getIs_completed
+  };
 
-  await model.insertMany([planDocument])
-  res.send("berhasil menambahkan rencana")
-})
-
-app.get('/getAll', async(req,res) =>{
-  const result = await model.find();
-  res.send(result)
+  await model.insertMany([planDocument]);
+  res.json({
+    message: 'Berhasil menambahkan rencana',
+    data: planDocument
+  });
 });
 
-app.get('/getByTitle', async(req,res) => {
-  const result = await model.find({
-    title:req.body.title
-  })
+app.get('/getAll', async (req, res) => {
+  const result = await model.find();
+  res.json(result);
+});
 
-  res.send(result)
-})
+app.get('/getByTitle/:title', async (req, res) => {
+  const result = await model.find({
+    title: req.params.title
+  });
+
+  res.json(result);
+});
 
 app.listen(port, () => {
-    console.log(`Server running at ${baseUrl}`);
+  console.log(`Server running at ${baseUrl}`);
 });
