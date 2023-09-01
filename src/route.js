b96023bca66dbe7d544f20edc35c0fb61a1d9f5e
NaufalK25 <naufalkateni2001@gmail.com>
2023-09-01 21:09:45 +0700

    sort route path

--- src/route.js
+++ src/route.js
@@ -4,11 +4,11 @@ const controller = require('./controller');
 const router = express.Router();
 
 router.post('/create', controller.create);
+router.delete('/deleteById/:_id', controller.deleteById);
+router.put('/update/:_id', controller.updateById);
 router.get('/getAll', controller.getAll);
 router.get('/getByTitle/:title', controller.getByTitle);
 router.get('/getById/:_id', controller.getById);
-router.delete('/deleteById/:_id', controller.deleteById);
-router.put('/update/:_id', controller.updateById);
 router.get('/', controller.getRoot);
 
 module.exports = router;
