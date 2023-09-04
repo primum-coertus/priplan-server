cd29ff3e904282d92d04bd15c570594577d4767e
NaufalK25 <naufalkateni2001@gmail.com>
2023-09-04 22:11:46 +0700

    feat: change default port to 3001

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const route = require('./route');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const baseUrl = process.env.BASE_URL || `http://localhost:${port}`;
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/priplan');
 
